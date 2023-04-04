aaeb6c8a0e20f08be44028cb8e303a8d43fe13eb
pablogd731 <pablogd731@gmail.com>
2023-04-03 20:24:00 -0600

    se agrego el codigo y el token

--- script.R
+++ script.R
@@ -2,9 +2,11 @@
 
 #Instalar librerias
 install.packages("rio")
+install.packages("stringr")
 
 #Usar librerias 
 library(rio)
+library(stringr)
 
 #Directorio local 
 setwd("//home//pablogd731//Bank_analyst")
@@ -12,4 +14,5 @@ setwd("//home//pablogd731//Bank_analyst")
 #Token de API 
 token <- import("Token", "txt")
 
-
+#Web
+web <- str_c("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=",token)
