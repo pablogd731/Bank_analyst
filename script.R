6b9dae420ec61ac81b0d20658d542a14930523aa
pablogd731 <pablogd731@gmail.com>
2023-04-03 21:01:21 -0600

    revisando un ejemplo de un banco

--- script.R
+++ script.R
@@ -3,10 +3,12 @@
 #Instalar librerias
 install.packages("rio")
 install.packages("stringr")
+install.packages("jsonlite")
 
 #Usar librerias 
 library(rio)
 library(stringr)
+library(jsonlite)
 
 #Directorio local 
 setwd("//home//pablogd731//Bank_analyst")
@@ -14,5 +16,11 @@ setwd("//home//pablogd731//Bank_analyst")
 #Token de API 
 token <- import("Token", "txt")
 
-#Web
-web <- str_c("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=",token)
+#Web Bank of America
+web <- str_c("https://www.alphavantage.co/query?function=OVERVIEW&symbol=BAC&apikey="
+             ,token)
+
+#Analisis
+datos <- fromJSON(web)
+
+
