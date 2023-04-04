e831341266406375f5dfe36babcb30ed9d5eb214
Pablo Garcia <pablogd731@gmail.com>
2023-04-03 19:55:50 -0600

    se agrego el codigo y el token

---
+++ script.R
@@ -0,0 +1,15 @@
+#API alpha vantage
+
+#Instalar librerias
+install.packages("rio")
+
+#Usar librerias 
+library(rio)
+
+#Directorio local 
+setwd("//home//pablogd731//Bank_analyst")
+
+#Token de API 
+token <- import("Token", "txt")
+
+
